69aaa0eb0042cd5fc6796f0942637acdcaf4caa5
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-10-17 15:24:13 -0500

    started work on addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+
+function addToCollection(title, artist, yearPublished){
+  this.title = title;
+  this.artist = artist;
+  this.yearPublished = yearPublished;
+    collection.push(addToCollection);
+    return collection;
+}
+addToCollection('Go Farther In Lightness', 'Gang of Youths', '2007');
+  console.log(collection);
+
