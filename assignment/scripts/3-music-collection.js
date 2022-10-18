87ad5c22fd14a26cdf331753941ebc52dbe9e55c
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-10-18 15:30:18 -0500

    trying to figure out findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,15 +6,6 @@ function addToCollection(title, artist, yearPublished){
  return collection.push({title, artist, yearPublished})
 }
 
-let album= {
-  title: this.title,
-  artist: this.artist,
-  yearPublished: this.yearPublished,
-  addToCollection: function(){
-    return collection.push(this.title + this.artist + this.yearPublished)
-  }
-};
-
 addToCollection("In Rainbows", "Radiohead", "2007");
 addToCollection("Go Farther In Lightness", "Gang of Youths", "2017");
 addToCollection("Hot Fuss", "The Killers", "2004");
@@ -23,16 +14,25 @@ addToCollection("RTJ4", "Run The Jewels", "2020");
 addToCollection("I Don\'t Live Here Anymore", "The War On Drugs", "2021");
 console.log(collection);
 
-
 function showCollection(array){
-  title = album.title;
-  artist = album.artist;
-  yearPublished = album.yearPublished;
-  let total = array.length;
-  console.log(total);
-  for (i = 0; i < array.length; i++) {
-    console.log(array[i].title + " by " + array[i].artist + " published in " + array[i].yearPublished)
+  console.log(array.length);
+  for (i = 0; i < array.length; i++){
+    console.log(array[i].title + " by " + array[i].artist + " published in " + array[i].yearPublished);
   }
-};
-
+}
 showCollection(collection);
+
+let newList = [];
+
+function findByArtist(artist){
+  let chars = collection[artist];
+  for (let i = 0; i < collection.length; i++) {
+    if (chars === "") {
+     return newList.push(chars);
+  } else {
+    return [];
+  }
+}
+}
+findByArtist("Radiohead");
+console.log(newList);
